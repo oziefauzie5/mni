ba82d21460db8f06db9af1541a040a318579e535
oziefauzie5 <oziefauzie31@gmail.com>
2024-11-05 00:44:12 +0700

    mni2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,7 +7,7 @@
       <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
         <div>
           <h2 class="text-white pb-2 fw-bold">{{Session::get('app_nama')}}</h2>
-          <h5 class="text-white op-7 mb-2">{{Session::get('app_brand')}}</h5>
+          <img src="{{ asset('storage/img/'.Session::get('app_logo')) }}" style="width: 90%;" alt="Logo Brand" class="navbar-brand">
         </div>
       </div>
     </div>
@@ -15,20 +15,3 @@
 </div>
 
 @endsection
-
-<script>
-  	Circles.create({
-			id:'circles-1',
-			radius:45,
-			value:60,
-			maxValue:100,
-			width:7,
-			text: 5,
-			colors:['#f1f1f1', '#FF9E27'],
-			duration:400,
-			wrpClass:'circles-wrp',
-			textClass:'circles-text',
-			styleWrapper:true,
-			styleText:true
-		})
-</script>
